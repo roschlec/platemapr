0036a0b571d1b4aaeb9a7a715ccdb2865718f881
roschlec <roschlec@gmail.com>
2024-09-01 18:30:28 +0200

    Update

---
+++ R/create_treatment.R
@@ -0,0 +1,8 @@
+create_treatment <- function(type_well, rep) {
+
+  plate_trt <- paste("trt", rep(seq(1:(well[[type_well]][['n']]/rep)), rep), sep = "_")
+  extra <- well[[type_well]][['n']] - length(plate_trt)
+  plate_trt <- c(plate_trt, rep("empty", times = extra))
+
+  return(plate_trt)
+}
